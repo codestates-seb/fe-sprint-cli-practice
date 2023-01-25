bf294e429dd3e784884262016752a278a940d8ab
BlankCode <110416295+BlankCodeStack@users.noreply.github.com>
2023-01-25 13:39:39 +0900

    김희환

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -2,6 +2,23 @@ const range = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
   // TODO
+  let count =' ';
+    let a;
+    let output = [];
+        if (upTo<=1) {
+            count = count;
+        }
+        else if(upTo === 2) {
+            count = '2';
+        }
+        else {
+            count = '2';
+            for(let i = 4; i <= upTo; i = i + 2) {
+                count = count + ',' + i;    
+            }
+        }
+        a = count.split(',').map(parseFloat);
+        return a;
 }
 
 module.exports = getListMultiplesOfTwo;
