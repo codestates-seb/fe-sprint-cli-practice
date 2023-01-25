61bf4f51be357cca2c1835dbe2cb515ce6cb0533
naim <rlaskdla97@gmail.com>
2023-01-25 14:46:23 +0900

    과제 제출 합니다 [김나임]

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -1,7 +1,17 @@
 const range = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
-  // TODO
+  
+  let arr = [];
+
+  for(let i = 1; i <= upTo; i++) {
+    if(i % 2 === 0) {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+  
 }
 
 module.exports = getListMultiplesOfTwo;
